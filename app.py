ea040f870720f419f08c168868553d2fa2b1bd55
Erika Diaz <erikaitzeldiaz@gmail.com>
2024-03-07 03:18:37 -0600

    added post delete

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import datetime
 import os
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from bson import ObjectId
 
 load_dotenv()
 def create_app():
@@ -18,13 +19,24 @@ def create_app():
             app.db.entries.insert_one({"content":entry_content,"date":formatted_date})
         entries_with_date = [
             (
+                entry["_id"],
                 entry["content"],
                 entry["date"],
                 datetime.datetime.strptime(entry["date"],"%y-%m-%d").strftime("%b %d")
             )
             for entry in app.db.entries.find({})
         ]
-        return render_template("home.html",entries=entries_with_date)
+        latest_first = entries_with_date.copy()
+        latest_first.reverse()
+        return render_template("home.html",entries=latest_first)
+
+    @app.route('/delete/<entry_id>', methods=['POST', 'DELETE'])
+    def delete_entry(entry_id):
+        if request.method in ['POST', 'DELETE']:
+            entry_id_obj = ObjectId(entry_id)
+            app.db.entries.delete_one({'_id': entry_id_obj})
+        return redirect(url_for('home'))
+
     @app.route("/calendar/")
     def calendar():
         return render_template("comingSoon.html")
